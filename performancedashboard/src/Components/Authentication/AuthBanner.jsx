2ca0eff9f59c2ade974abea7f84e9c2938d57205
Essien Bassey <basseyalison@gmail.com>
2024-03-09 19:13:05 -0600

    Performance Report project - Essien - Added Authentication and Registration banner

---
+++ performancedashboard/src/Components/Authentication/AuthBanner.jsx
@@ -0,0 +1,13 @@
+
+
+function AuthBanner () {
+    return (
+        <div className="auth-banner">
+            <div className="auth-banner__content">
+                <h1 className="auth-banner__title">Performance Dashboard</h1>
+                <p className="auth-banner__subtitle">Please login to continue</p>
+            </div>
+        </div>
+    )
+}
+export default AuthBanner
